e47d7b899c9e1ff4047482d852b5fc5ced6bb9dc
Vinisantas <viniciuspereirasantana@gmail.com>
2026-04-22 17:59:42 -0300

    adicionado base para schema e service de estoque

--- schemas/estoque_schema.py
+++ schemas/estoque_schema.py
@@ -1,7 +1,31 @@
 from pydantic import BaseModel
 
-
 class EstoqueBase(BaseModel):
     produto_id: int
     unidade_id: int
     quantidade: int
+
+
+class EstoqueConsulta(BaseModel):
+    produto_id: int
+    unidade_id: int
+    quantidade: int
+
+class EstoqueCreate(BaseModel):
+    produto_id: int
+    unidade_id: int
+    quantidade: int
+
+
+class EstoqueUpdate(BaseModel):
+    produto_id: int | None = None
+    unidade_id: int | None = None
+    quantidade: int | None = None
+
+
+class EstoqueResponse(BaseModel):
+    produto_id: int
+    unidade_id: int
+    quantidade: int
+    class Config:
+        from_attributes = True
---
+++ services/estoque_service.py
@@ -0,0 +1,22 @@
+
+
+
+##função para adicionar e cadastrar estoque por unidade
+def adicionar_estoque():
+    pass
+
+##função para consultar estoque por unidade
+def consultar_estoque_por_unidade():
+    pass
+
+##função para consultar estoque por produto
+def consultar_estoque_por_produto():
+    pass
+
+#validar antes do pedido (importante!)
+def verificar_disponibilidade():
+    pass
+
+#reduzir ao vender
+def dar_baixa_estoque():
+    pass
